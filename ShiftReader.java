5761268b35b7080052bfa61556021604e83bd1f6
Robert Lester <robertlesterjr@mac.com>
2016-03-21 14:51:59 -0400

    Update ShiftReader.java

--- ShiftReader.java
+++ ShiftReader.java
@@ -1,12 +1,13 @@
 /* Shift Reader app for Arduino to test shift reading theory against Clarion data bus */
 
-volatile byte dataIn[8];
+volatile in dataIn[8];
 
 int dt_pin = 2;
 int cl_pin = 7;
 
 volatile int counter = 0;
 volatile bool receiving = false;
+volatile bool sending = false;
 
 void setup() {
   pinMode(dt_pin, INPUT_PULLUP);
@@ -14,8 +15,26 @@ void setup() {
   Serial.begin(9600);
 }
 
+void send(byte data) {
+  detachInterrupt(digitalPinToInterrupt(cl_pin));
+  shiftOut(dt_pin, cl_pin, MSBFIRST, data);
+  attachInterrupt(digitalPinToInterrupt(cl_pin), shift_dt, FALLING);
+}
+
 void loop() {
-  
+  if (!receiving && sending) {
+    sending = false;
+    byte value = bitsToByte(dataIn);
+    if (value == 0xF7) {
+      send(value);
+    } else {
+      if (value == 0x00) {
+        send(0xF7);
+      } else {
+        send(value);
+      }
+    }
+  }
 }
 
 void shift_dt() {
@@ -29,13 +48,14 @@ void shift_dt() {
   } else if (counter >= 144) {
     counter = 0;
     receiving = false;
+    sending = true;
   } else {
     digitalWrite(dt_pin, HIGH);
   }
   counter++;
 }
 
-byte bitsToByte(byte bits[8]) {
+byte bitsToByte(int bits[8]) {
   int bitmask[8] = {128,64,32,16,8,4,2,1};
   int result = 0;
   for (int i = 0; i < 8; i++) {
